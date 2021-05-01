054b956f946282812796ebd18fb8474b2a6546ee
RifqiSubchan02 <rifqisubchan@gmail.com>
2021-05-02 02:44:13 +0700

    save before try migrations with sequelize

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { userValidator, verifyToken, setMulter } = require('../config/middleware');
+const { userValidator, verifyToken } = require('../config/middleware');
 const { getAllUsers, getUserByID, userLogin, userRegister, userUpdated, userDeleted } = require('../controllers/user');
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:token', verifyToken, getUserByID);
-router.post('/login', userLogin)
+router.post('/login', userLogin);
 router.post('/', userValidator, userRegister);
 router.put('/:token', verifyToken, userValidator, userUpdated);
 router.delete('/:token', verifyToken, userDeleted);
